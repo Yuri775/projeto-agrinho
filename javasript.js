474a00d39c6ed3cc7f9780a8660001cf521f242e
Yuri775 <yuri.silvestrini@escola.pr.gov.br>
2025-05-30 20:38:50 +0000

    Adicionando interruptor

--- javasript.js
+++ javasript.js
@@ -1,8 +1,19 @@
 let luz = true;
+
 function acendeLuz() {
+
     if (luz == true) {
         luz = false;
+        document.documentElement.style.setProperty('--fundo', 'rgb(29, 29, 29)');
+        document.documentElement.style.setProperty('--fundo-cabecalho', '#ffffff');
+        document.documentElement.style.setProperty('--letra', '#ffffff');
+        document.documentElement.style.setProperty('--letra-cabecalho', 'black');
+        document.d
     } else {
         luz = true;
+        document.documentElement.style.setProperty('--fundo', '#ffffff');
+        document.documentElement.style.setProperty('--fundo-cabecalho', 'rgb(29, 29, 29)');
+        document.documentElement.style.setProperty('--letra', 'black');
+        document.documentElement.style.setProperty('--letra-cabecalho', '#ffffff');
     }
 }
